d4a5550724f9ec6d23a74b98214c2fcb60f642e8
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-21 13:58:30 -0700

    push

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,6 +57,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,7 +98,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -148,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import django_heroku
 django_heroku.settings(locals())
--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Room, Player
 # Register your models here.
+class RoomAdmin(admin.ModelAdmin):
+    readonly_fields = ('title', 'description')
+
+class PlayerAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', 'currentRoom')
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Player, PlayerAdmin)
